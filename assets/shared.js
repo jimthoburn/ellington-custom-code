d97896af12c5b33223d576c984645e3ffb5a47eb
Jim Thoburn <jim@jimthoburn.com>
2017-11-15 13:25:48 -0800

    Update code to work without line breaks (for school loop)

--- assets/shared.js
+++ assets/shared.js
@@ -1,5 +1,5 @@
 
-  // OPTIONAL: Remove the custom style sheet, if the user is signed in
+  /* OPTIONAL: Remove the custom style sheet, if the user is signed in */
   (function() {
     var timer;
     function check() {
@@ -12,50 +12,149 @@
       }
     }
 
-    // Check frequently
+    /* Check frequently */
     timer = setInterval(check, 10);
 
-    // Check one last time when the DOM has loaded
+    /* Check one last time when the DOM has loaded */
     document.addEventListener('DOMContentLoaded', function() {
       check();
       clearInterval(timer);
     });
-    // Stop checking when everything has loaded
+    /* Stop checking when everything has loaded */
     document.addEventListener('load', function() {
       clearInterval(timer);
     });
 
-    // If all else fails, stop checking after five seconds
+    /* If all else fails, stop checking after five seconds */
     setTimeout(function() {
       clearInterval(timer);
     }, 5000);
   })();
 
 
-  // OPTIONAL: Increase the resolution of the image in the header, by changing its “src” attribute
+  /* OPTIONAL: Increase the resolution of the image in the header, by changing its “src” attribute */
+  (function() {
+    if (!document.querySelector) {
+      return;
+    }
+
+
+    function updateImage() {
+      /* These selectors were taken directly from the style sheet, “custom.css” */
+      var image = document.querySelector(
+        '#container_content_home > .content_full > div > div[style="width: 100%; width: 970px"] > img[width="970"]:first-of-type'
+        + ', ' +
+        '#page_title + table > tbody > tr > td > #block_hub_main_b > .block_content_main > div > div[style="width: 100%; width: 350px"] > img[width="350"]:first-of-type'
+        + ', ' +
+        '#page_title + table > tbody > tr > td > #block_hub_main > .block_content_main > div > div[style="width: 100%; width: 465px"] > img[width="465"]:first-of-type'
+        + ', ' +
+        '#page_title + #block_wide_main > .block_content_main > div > div[style="width: 100%; width: 700px"] > img[width="700"]:first-of-type'
+      );
+      if (image) {
+        var url = image.getAttribute('src').
+                    replace('w350', 'w2000').
+                    replace('w465', 'w2000').
+                    replace('w700', 'w2000').
+                    replace('w970', 'w2000');
+
+        image.setAttribute('src', url);
+      } else {
+        /* If there’s no image, adjust the height of the header */
+        document.body.classList.add('azusa-no-header-image');
+      }
+    }
+
+    var timer;
+    function elementLoaded(element) {
+      if (element.parentNode && element.parentNode.nextSibling) {
+        return true;
+      } else {
+        return elementLoaded(element.parentNode);
+      }
+    }
+    function check() {
+      var title = document.getElementById('page_title');
+
+      if (title && elementLoaded(title)) {
+        updateImage();
+        clearInterval(timer);
+        timer = undefined;
+      }
+    }
+
+    /* Check frequently */
+    timer = setInterval(check, 10);
+
+    /*  Check one last time when the DOM has loaded */
+    document.addEventListener('DOMContentLoaded', function() {
+      if (timer) {
+        check();
+        if (timer) {
+          clearInterval(timer);
+        }
+      }
+    });
+
+    /* Stop checking when everything has loaded */
+    document.addEventListener('load', function() {
+      if (timer) {
+        check();
+        if (timer) {
+          clearInterval(timer);
+        }
+      }
+    });
+
+    /* If all else fails, stop checking after five seconds */
+    setTimeout(function() {
+      if (timer) {
+        check();
+        if (timer) {
+          clearInterval(timer);
+        }
+      }
+    }, 5000);
+  })();
+
+
+  /* OPTIONAL: Increase the resolution of all of the images at the beginning of the home page, and turn them into a slide show */
   document.addEventListener('DOMContentLoaded', function() {
-    // Check to see if the page HTML matches a certain pattern (the HTML that we expect to see on School Loop 1.0)
-    // If it does match, look for an image that appears right after the main headline
-    // If that image is found, update it’s URL from wXXX to w2000
 
-    // These selectors were taken directly from the style sheet, “custom.css”
-    var image = document.querySelector(
-      '#container_content_home > .content_full > div > div[style="width: 100%; width: 970px"] > img[width="970"]:first-of-type'
-      + ', ' +
-      '#page_title + table > tbody > tr > td > #block_hub_main_b > .block_content_main > div > div[style="width: 100%; width: 350px"] > img[width="350"]:first-of-type'
-      + ', ' +
-      '#page_title + table > tbody > tr > td > #block_hub_main > .block_content_main > div > div[style="width: 100%; width: 465px"] > img[width="465"]:first-of-type'
-      + ', ' +
-      '#page_title + #block_wide_main > .block_content_main > div > div[style="width: 100%; width: 700px"] > img[width="700"]:first-of-type'
+    /* These selectors were taken directly from the style sheet, “custom.css” */
+    var images = document.querySelectorAll(
+      '#container_content_home > .content_full > div > div[style="width: 100%; width: 970px"] > img[width="970"]'
     );
-    if (image) {
-      var url = image.getAttribute('src').
-                  replace('w350', 'w2000').
-                  replace('w465', 'w2000').
-                  replace('w700', 'w2000').
-                  replace('w970', 'w2000')
 
-      image.setAttribute('src', url);
-    }        
+    /* If there’s only one image, stop here */
+    if (images.length <= 1) {
+      return;
+    }
+
+
+    var nextImage;
+    var nextURL;
+    for (var index = 0; index < images.length; index++) {
+      nextImage = images[index];
+      nextURL = nextImage.getAttribute('src').replace('w970', 'w2000');
+
+      nextImage.setAttribute('src', nextURL);
+      if (index !== 0) {
+        nextImage.style.opacity = 0;
+      }
+    }
+
+    var currentIndex = 0;
+    function showNext() {
+      images[currentIndex].style.opacity = 0;
+
+      currentIndex++;
+      if (currentIndex >= images.length) {
+        currentIndex = 0;
+      }
+
+      images[currentIndex].style.opacity = 1;
+    }
+
+    var timer;
+    timer = setInterval(showNext, 7000);
   });
-
